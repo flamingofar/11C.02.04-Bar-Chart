70bb9814e7e1a9f81cac949e4e74a6e5c41d9be5
malte <malt0533@stud.kea.dk>
2022-02-09 15:52:25 +0100

    All working

--- js/index.js
+++ js/index.js
@@ -5,24 +5,46 @@
 console.log("Hi from barchart");
 
 let data = [];
-// Create mock data
-function mockData() {
-	for (let i = 0; i < 40; i++) {
-		let ran = Math.floor(Math.random() * 32) + 1;
-		data.push(ran);
-	}
-	showData();
+let count = 0;
+let scrollSpeed = 500;
+
+function start() {
+	setTimeout(showData, scrollSpeed);
 }
 
-// Visualize data
-function showData() {
+// Visualize data with 32 is 100% and every entry is relative to 32
+function appendData() {
 	const barChart = document.querySelector(".bar_chart");
-	data.forEach((el) => {
-		barChart.innerHTML += `<div class="bar" style= "height: ${
-			(el / data.length) * 100
-		}%;"></div>`;
-	});
+	barChart.innerHTML += `<div class="bar" style= "height: ${
+		(data[0] / 32) * 100
+	}%;"></div>`;
 }
 
-mockData();
-console.log(data);
+// Get The Data
+function getData() {
+	const ran = getNumberOfCustomers();
+
+	if (count < 40) {
+		data.unshift(ran);
+		count++;
+	} else {
+		data.length = 39;
+		data.unshift(ran);
+	}
+	console.log(data);
+	start();
+}
+
+// Show the data all together
+function showData() {
+	getData();
+	appendData();
+}
+
+// Get number of customers
+function getNumberOfCustomers() {
+	// Fake amount of guests. Max 32
+	return Math.floor(Math.random() * 32);
+}
+
+start();
